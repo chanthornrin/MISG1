1a3a9238c734b6cda6cb0d24c0071a3e37869418
S7799 <88360723+S7799@users.noreply.github.com>
2021-08-03 21:21:58 +0700

    Create variable

--- Inventory Mangement Systems/Inventory Mangement Systems/CreateUser.vb
+++ Inventory Mangement Systems/Inventory Mangement Systems/CreateUser.vb
@@ -1,5 +1,6 @@
 ﻿Public Class CreateUser
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim chanthorn As String
+        Dim Phearak As String
     End Sub
 End Class
